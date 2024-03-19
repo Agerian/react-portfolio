216c0d4c3333bb8095f6f3ef2c87be70790ca1ad
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-03-19 11:29:03 -0600

    Add Header component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <div>
+          <Header />
 
+        </div>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>AdrianO</h1>
+    </header>
+  )
+}
+
+export default Header;
