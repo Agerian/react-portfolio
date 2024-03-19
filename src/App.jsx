1b3ba7789fb274021d147251150915c17333347d
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-03-19 16:19:12 -0600

    Add Contact and Resume components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 //import './App.css'
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
             <Routes>
               <Route path="/" element={<AboutMe />} />
               <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/resume" element={<Resume />} />
             </Routes>
           </main>
 
---
+++ src/components/Resume.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div>
+      
+
+      <h3>Front-end Proficiencies</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+
+      <h3>Back-end Proficiencies</h3>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+      </ul>
+
+      <h2>Resume</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum enim rem distinctio placeat officiis tempora commodi repellat nulla pariatur aspernatur.</p>
+      <a href="path/to/resume.pdf" target="_blank" rel="noopener noreferrer" className="download-resume">Download Resume</a>
+    </div>
+    
+  )
+}
