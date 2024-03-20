8283eda04b6828cccb1be06806c6e827f842b4aa
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-03-19 19:52:00 -0600

    Update AboutMe component and add AboutMe.css

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import self1 from '../assets/images/self1.webp';
+import self1 from '../assets/images/self2.webp';
+import '../styles/AboutMe.css';
 
 function AboutMe() {
   return (
-    <div>
-      <img src={self1} alt="Agerian" />
+    <div className="about-me">
+      <img src={self1} alt="Agerian" className="self"/>
       <h2>About Me</h2>
       <p>
         I am a full stack web developer with a passion for creating and learning. I have experience with HTML, CSS, JavaScript, Node.js, Express.js, MySQL, MongoDB, and React. I am a quick learner and I am always looking for new challenges. I am currently working on a project that uses the MERN stack and I am excited to see where it takes me.
